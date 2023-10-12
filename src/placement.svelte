8d48cc06c255e58befa7ae28f8ab07a5d8f8de1c
Yugandhar182 <132258244+Yugandhar182@users.noreply.github.com>
2023-10-12 11:05:34 +0530

    Add files via upload

---
+++ src/placement.svelte
@@ -0,0 +1,24 @@
+<script>
+    let data = null;
+  
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://api.recruitly.io/api/dashboard/ats/data/placementmonthlymetrics?start=01%2F01%2F2023&end=11%2F10%2F2023&apiKey=TEST45684CB2A93F41FC40869DC739BD4D126D77');
+        if (response.ok) {
+          data = await response.json();
+          console.log(data);
+        } else {
+          console.error('Failed to fetch data from the API');
+        }
+      } catch (error) {
+        console.error('An error occurred:', error);
+      }
+    };
+  </script>
+  
+  <main>
+    <h1>Svelte API Demo</h1>
+    <button on:click={fetchData}>Fetch Data</button>
+    <pre>{JSON.stringify(data, null, 2)}</pre>
+  </main>
+  
